43bd751a1197ad3eef0c2e428e16bec03513e58b
Benoît Dubreuil <b.dubreuil@hotmail.com>
2021-02-11 14:55:33 -0500

    Added kill_video_app.bat

---
+++ clean_bloat_processes/kill_bloat_processes/commands/kill_video_app.bat
@@ -0,0 +1,9 @@
+@echo off
+setlocal
+
+echo [91mKilling[0m Skype
+
+call lib_kill.bat kill_tasks_as_cvs "tasklist /nh /apps /fo csv ^| grep -i video"
+call lib_kill.bat kill_tasks_as_cvs "tasklist /nh /svc /fo csv ^| grep -i video"
+
+exit /b
