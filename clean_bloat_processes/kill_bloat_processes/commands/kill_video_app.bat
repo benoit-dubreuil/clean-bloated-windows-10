17e681cc22dd50b95abe40657a65406a49b0a7ff
Benoît Dubreuil <b.dubreuil@hotmail.com>
2021-02-11 19:00:10 -0500

    Fixed bad copy paste

--- clean_bloat_processes/kill_bloat_processes/commands/kill_video_app.bat
+++ clean_bloat_processes/kill_bloat_processes/commands/kill_video_app.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 
-echo [91mKilling[0m Skype
+echo [91mKilling[0m Video App
 
 call lib_kill.bat kill_tasks_as_cvs "tasklist /nh /apps /fo csv ^| grep -i video"
 call lib_kill.bat kill_tasks_as_cvs "tasklist /nh /svc /fo csv ^| grep -i video"
